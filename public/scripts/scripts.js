f6f323ed8784358de86783ae2390d7a314e92c61
Jack Laird <jack.laird0@gmail.com>
2018-06-29 10:15:44 -0600

    Setup functionality

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -4,6 +4,8 @@ const $colorCard2 = $('.color-card2');
 const $colorCard3 = $('.color-card3');
 const $colorCard4 = $('.color-card4');
 const $colorCard5 = $('.color-card5');
+const $projectInput = $('.project-input');
+const $saveProjectButton = $('.save-project');
 
 const colorValues = ['a', 'b', 'c', 'd', 'e', 'f', 
                       0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -15,6 +17,23 @@ const randomColorGenerator = () => {
   }
    return color
 } 
+
+const submitProjectName = (projectName) => {
+  console.log('success')
+  fetch('http://localhost:5280/projects/', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({project: {name: projectName}})
+  });
+}
+
+$saveProjectButton.on('click', event => {
+  event.preventDefault
+  submitProjectName($projectInput.val())
+  }
+);
 
 const generateNewPalette = (e) => {
   e.preventDefault();
